ba1818d2d19fbf4082ed60b72cdff3fec5acd6f2
Ro <rkcheruk@ucsc.edu>
2024-05-15 23:58:12 -0700

    Finished Intro Section

--- TestingUnity_First/Assets/PrintToConsole.cs
+++ TestingUnity_First/Assets/PrintToConsole.cs
@@ -7,7 +7,7 @@ public class PrintToConsole : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("NEED TO CATCH THE BOLT");
     }
 
     // Update is called once per frame
