cde35ceed56cc820ffc766750b8524050c0569e6
Ro <rkcheruk@ucsc.edu>
2024-05-16 15:51:29 -0700

    Added variables to the C Sharp Script

--- DeliveryDriver/Assets/Driver.cs
+++ DeliveryDriver/Assets/Driver.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,10 @@ using UnityEngine.UIElements;
 
 public class Driver : MonoBehaviour
 {
+    // Global Class Variables
+    [SerializeField] float steerSpeedScale = 1f;
+    [SerializeField] float moveSpeedScale = 0.1f;
+
     // Start is called before the first frame update
     void Start()
     {   
@@ -13,11 +18,14 @@ public class Driver : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeedScale;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeedScale;
+
         // rotates the car on the z axis
-        transform.Rotate(0, 0, -0.2f);
+        transform.Rotate(0, 0, -steerAmount);
         
         // moves the car forward
-        transform.Translate(0, 0.01f, 0);
+        transform.Translate(0, moveAmount, 0);
     }
 }
