adc0e20179770571f110c07e2afc1c7660ac8075
Ro <rkcheruk@ucsc.edu>
2024-05-22 22:10:45 -0700

    Added colliders and triggers to finish line and when you hit your head. Will work on this more soon

---
+++ SnowBoarder/Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashDetector : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Head_Death"){
+            Debug.Log("Ooops, you DIED!!!");
+        }
+    }
+}
